a455f3f26e778e876c7e909c947f801c4d99b9ab
sumit phadke <sumitphadke2001@gmail.com>
2021-07-10 23:45:18 +0530

    final commit

--- listprobl.py
+++ listprobl.py
@@ -2,14 +2,26 @@
 # print(n)
 # for i in range(n):
 #     print()
-l=[]
-n=input("").split()
-print(n)
-if n[0]=="insert":
-    l.insert(int(n[1]),int((n[2])))
-print(l)
-s=input("").split()
-print(s)
-if s[0]=="remove":
-    l.remove(int(s[1]))
-print(l)
+s=int(input("input"))
+l = []#empty list
+for i in range(s):#it will execute 4 times
+    n = input("").split()#split method for eg(insert 0 5)means 5 at 0 index
+    # print(n)#print the input
+    if n[0] == "insert":#the if else part
+        l.insert(int(n[1]), int((n[2])))
+        # print(l)
+    elif n[0]=="remove":#this is remove it will remove number at the specific index
+        l.remove(int(n[1]))#int(n[1]) it will convert the string into integer
+        # print(l)
+    elif n[0]=="print":
+        print(l)
+    elif n[0]=="append":
+        l.append(int(n[1]))
+    elif n[0]=="sort":
+        l.sort()
+    elif n[0]=="reverse":
+        l.reverse()
+    elif n[0]=="pop":
+        l.pop()
+
+# print(l)
